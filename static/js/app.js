ee0dc7bd74d183f9cb178eb38cca4cd691871169
alanacsaposs <alanacsaposs@gmail.com>
2019-09-22 22:53:11 -0700

    Cleaned up code and comments

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
 // Console.log the data from data.js
 console.log(data);
 
-// // Use d3 to update each cell's text with
-// // UFO sighting information
-  data.forEach(function(ufoSighting) {
+// Update each cell's text with
+// UFO sighting information
+data.forEach(function(ufoSighting) {
   console.log(ufoSighting);
   var row = tbody.append("tr");
   Object.entries(ufoSighting).forEach(function([key, value]) {
@@ -18,14 +17,16 @@ console.log(data);
 // // Append a cell to the row for each value
   var cell = row.append("td");
   cell.text(value);
-  });
+    });
   });
 
 // Select Filter Button
 var button = d3.select("#filter-btn");
+
+// Create Button function
 button.on("click", function() {
 
-  // Select the input element and get the raw HTML node
+    // Select the input element and get the raw HTML node
     var inputElement = d3.select(".form-control");
   
     // Get the value property of the input element
@@ -33,20 +34,20 @@ button.on("click", function() {
   
     console.log(inputValue);
   
+    // Filter the data by inputValue
     var filteredData = data.filter(data => data.datetime === inputValue);
   
     console.log(filteredData);
 
-    // remove any all previous data from table
+    // Remove any all previous data from table
     tbody.html("");
 
-  // append stats to the list
+    // Append table with new data
     filteredData.forEach(function(ufoSighting) {
       console.log(ufoSighting);
       var row = tbody.append("tr");
       Object.entries(ufoSighting).forEach(function([key, value]) {
       console.log(key, value);
-  // // Append a cell to the row for each value
       var cell = row.append("td");
       cell.text(value);
       });
